864e4cb2483b422bd58cebfda3dab7113c5740fd
vijay-talsangi <vijaytalsangi4705@gmail.com>
2025-08-30 13:59:50 +0530

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProblemStatements from './ProblemStatements';
 import Teams from './team';
 function App() {
